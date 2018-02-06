3dea371a6122b20d283c563ab0ee33fad67c67bd
dewqwer <35560676+dewqwer@users.noreply.github.com>
2018-02-06 13:40:01 +0000

    edit_variable_name
    max-->a
    min-->b

--- src/myself/Myself.java
+++ src/myself/Myself.java
@@ -1,28 +1,16 @@
-package myself;
-
 import java.util.Scanner;
+public class Myself 
+{    public static void main(String[] args) {        
+    byte a = Byte.MAX_VALUE;        
+    byte b = Byte.MIN_VALUE;       
+        System.out.println("max byte = " + a);        
+    System.out.println("max byte = " + b);        
+    System.out.println("");        
+    System.out.println("max+1 = " + ++a);        
+    System.out.println("min-1 = " + --b);        
+    System.out.println("");     
+        byte num = 0;        
+    for (int i = 0; i <= 149; i++) {           
+        System.out.println("i = " + i + " num = " + num);            
+        num++;       }
 
-public class Myself {
-
-    public static void main(String[] args) {
-
-        byte max = Byte.MAX_VALUE;
-        byte min = Byte.MIN_VALUE;
-
-        System.out.println("max byte = " + max);
-        System.out.println("max byte = " + min);
-        System.out.println("");
-        System.out.println("max++ = " + max++);
-        System.out.println("min-- = " + max--);
-        System.out.println("");
-
-        byte num = 0;
-        for (int i = 0; i <= 149; i++) {
-            System.out.println("i = " + i + " num = " + num);
-            num++;
-
-        }
-
-    }
-
-}
